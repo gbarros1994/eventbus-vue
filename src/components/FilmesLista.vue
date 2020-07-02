cd34fe4abe827714b52204897ccc94507a8f142d
gerson <gerson.barros@m2n.com.br>
2020-07-01 23:45:43 -0300

    passando dados via PROP

---
+++ src/components/FilmesLista.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row">
+
+    <!-- coluna 1 -->
+    <div class="col-8">
+
+      <h2>Filmes</h2>
+      {{filmeSelecionado}}
+
+      <div class="list-group list-group-flush">
+
+        <FilmesListaIten v-for="filme in filmes"
+        :key="filme.id"
+        :filme="filme"
+        :class="aplicarClasseAtive(filme)"
+        @selecionarFilme="filmeSelecionado = $event" />
+
+      </div>
+    </div>
+
+    <!-- coluna 2 -->
+    <div class="col-4">
+
+      <FilmesListaItenInfo :filme="filmeSelecionado" />
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import FilmesListaIten from './FilmesListaIten.vue'
+import FilmesListaItenInfo from './FilmesListaItenInfo.vue'
+
+export default {
+  components: {
+    FilmesListaIten,
+    FilmesListaItenInfo
+  },
+  data() {
+    return {
+      filmes: [
+       { id: 1, titulo: 'Naruto', ano: 2008 },
+       { id: 2, titulo: 'Bleach', ano: 2009 },
+       { id: 3, titulo: 'Harry potter', ano: 2015 },
+       { id: 4, titulo: 'Bob Esponja', ano: 2000 },
+      ],
+      filmeSelecionado: undefined
+    }
+  },
+  methods: {
+    aplicarClasseAtive(filmeIterado) {
+      return {
+        active: this.filmeSelecionado && this.filmeSelecionado.id === filmeIterado.id
+      }
+    }
+  },
+}
+</script>
---
+++ src/components/FilmesListaIten.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="list-group-item">
+        <span>{{ fontMaior }} | {{ filme.ano }}</span>
+        <button class="btn float-right" @click="selecionar">Selecionar</button>
+    </li>
+</template>
+
+<script>
+export default {
+    // inheritAttrs: false,
+    props: {
+        filme: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        fontMaior() {
+            return this.filme.titulo.toUpperCase()
+        }
+    },
+    methods: {
+        selecionar() {
+            this.$emit('selecionarFilme', this.filme)
+        }
+    },
+    // created() {
+    //     console.log(this.$attr)
+    // },
+}
+</script>
